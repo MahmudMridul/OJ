034c93152e3c7d047d99fde5ba95abb351179093
Abdullah Al Mahmud Mridul <mahmudmridul.185@gmail.com>
2021-03-28 11:32:43 +0600

    new functions added to Array class

--- Array.java
+++ Array.java
@@ -1,3 +1,7 @@
+
+import static java.lang.System.out;
+import java.util.Arrays;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +14,7 @@
  */
 public class Array 
 {
+    
     public int[] reverseArray(int[] arr)
     {
         int left = 0, right = arr.length-1;
@@ -32,5 +37,49 @@ public class Array
             max = Math.max(arr[i], max);
         }
         return new int[] {min, max};
+    }
+    
+    public int[] kth_maxmin(int[] arr, int k)
+    {
+        if(k>=0 && k<=arr.length)
+        {
+            Arrays.sort(arr);
+            return new int[] { arr[k+1], arr[arr.length-k] };
+        }
+        else
+        {
+            return new int[] { Integer.MAX_VALUE, Integer.MIN_VALUE };
+        }
+        
+    }
+    
+    public int[] arrayofZeroOneTwo(int[] ar)
+    {
+        int current = 0;
+        for(int elem = 0; elem<3;++elem)
+        {
+            for(int i=0;i<ar.length;++i)
+            {
+                if(ar[i]==elem)
+                {
+                    int temp = ar[current];
+                    ar[current] = ar[i];
+                    ar[i] = temp;
+                    ++current;
+                }
+            }
+        }
+        return ar;
+    }
+}
+
+class Run
+{
+    public static void main(String[] args) 
+    {
+        Array array = new Array();
+        int[] a = {1,2,0,2,1,0,2,1,1,2,0,1};
+        a = array.arrayofZeroOneTwo(a);
+        out.println(Arrays.toString(a));
     }
 }
