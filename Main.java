5ad15d9e29e59fdc4d48a9a02095b6e3546e2a2e
Abdullah Al Mahmud Mridul <mahmudmridul.185@gmail.com>
2021-04-05 12:46:14 +0600

    changes in reader class

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import static java.lang.System.out;
 import java.util.*;
 import java.util.Map.Entry;
@@ -11,82 +15,31 @@ import java.util.Map.Entry;
 public class Main 
 {
     
+    
     public static void main(String[] args) throws Exception
     {
-        /*
-        FileWriter writer = new FileWriter("input.txt");
-        writer.write(100+"\n");
-        for(int i=0;i<10;++i)
-        {
-            int val = Mother_Class.getRandomInteger(1, 1000000000);
-            writer.write(val+"\n");
-        }        
-        writer.close();*/
         
-
+        FileWriter writer = new FileWriter("input.txt");
+        for(int i=1;i<=10000000;++i)
+        {
+            long a = Mother_Class.getRandomInteger(1, 100000000);
+            long b = Mother_Class.getRandomInteger(1, 100000000);
+            writer.write(a+" "+b+"\n");
+        }        
+        writer.close();
+        
+        
         
         long start = System.currentTimeMillis();
         
         Reader read = new Reader("input.txt");
-        int test, n;
-        test = read.inte();
-        while(test-- > 0)
+        for(int i=0;i<200000;++i)
         {
-            n = read.inte();
-            char[] num = new char[n];
-            for(int i=0;i<n;++i) { num[i] = read.chrall(); }
-            
-            if(n==1) { out.println(-1); }
-            else
-            {
-                int size = n;
-                int ind = n-1;
-                int lastDigit = num[ind] - 48;
-                while(lastDigit%2==0)
-                {
-                    num[ind] = '$';
-                    --ind; --size;
-                    if(ind<0) { break; }
-                    lastDigit = num[ind] - 48;
-                }
-                if(ind<0) { out.println(-1); }
-                else
-                {
-                    int sum = 0;
-                    for(int i=0;i<=ind;++i)
-                    {
-                        sum += (num[i]-48);
-                    }
-                    if(sum%2==0) 
-                    {
-                        for(int i=0;i<=ind;++i)
-                        {
-                            out.print(num[i]);
-                        }
-                        out.println();
-                    }
-                    else
-                    {
-                        for(int i=ind;i>=0;--i)
-                        {
-                            if((num[i]-48)%2==1) { num[i] = '$'; --size; break; }
-                        }
-                        if(size>1)
-                        {
-                            for(int i=0;i<=ind;++i)
-                            {
-                                if(num[i]!='$') { out.print(num[i]); }
-                            }
-                            out.println();
-                        }
-                        else { out.println(-1); }
-                    }
-                }
-            }
+            long a = read.rlong(), b = read.rlong();
         }
+        
         long end = System.currentTimeMillis();
         out.println("Execution Time: "+(end-start)+"ms");
-        
     }
     
     
@@ -167,16 +120,20 @@ class Reader
         bufferPointer = bytesRead = 0;
     }
 
-    public String stren() throws IOException
+    public String rstr() throws IOException
     {
         byte[] buf = new byte[500000]; // line length
         int cnt = 0, c;
-        while ((c = read()) != -1) {
-            if (c == '\n') {
-                if (cnt != 0) {
+        while ((c = read()) != -1) 
+        {
+            if (c == '\n') 
+            {
+                if (cnt != 0) 
+                {
                     break;
                 }
-                else {
+                else 
+                {
                     continue;
                 }
             }
@@ -185,141 +142,84 @@ class Reader
         return new String(buf, 0, cnt);
     }
 
-    public String strsp() throws IOException
-    {
-        byte[] buf = new byte[500000]; // line length
-        int cnt = 0, c;
-        while ((c = read()) != -1) {
-            if (c == ' ' || c == '\n') {
-                if (cnt != 0) {
-                    break;
-                }
-                else {
-                    continue;
-                }
-            }
-            buf[cnt++] = (byte)c;
-        }
-        return new String(buf, 0, cnt);
-    }
-    
-    public char chrall() throws IOException
-    {
-        int c = read();
-        return (char)c;
-    }
-    
-    public char chrex(char ch) throws IOException
-    {
-        int c = read();
-        while(c==(int)ch)
-        {
-            c = read();
-        }
-        return (char)c;
-    }
-    
-    public int inte() throws IOException
+    public int rint() throws IOException
     {
         int ret = 0;
         byte c = read();
-        boolean eof = false;
-        while (c <= 47 || c >= 58) 
+        while (c <= ' ') 
         {
-            if(c==45) { break; }
-            if(c==-1) { eof = true; break; }
             c = read();
         }
-        if(eof) { return Integer.MIN_VALUE; }
-        
         boolean neg = (c == '-');
-        if (neg)
-            c = read();
-        do {
+        if (neg) { c = read(); }
+        do 
+        {
             ret = ret * 10 + c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
+        } 
+        while ((c = read()) >= '0' && c <= '9');
 
-        if (neg)
-            return -ret;
+        if (neg) { return -ret; }
         return ret;
     }
 
-    public long lon() throws IOException
+    public long rlong() throws IOException
     {
         long ret = 0;
         byte c = read();
-        boolean eof = false;
-        while (c <= 47 || c >= 58) 
-        {
-            if(c==45) { break; }
-            if(c==-1) { eof = true; break; }
-            c = read();
-        }
-        if(eof) { return Long.MIN_VALUE; }
-        
+        while (c <= ' ') { c = read(); }
         boolean neg = (c == '-');
-        if (neg)
-            c = read();
-        do {
+        if (neg) { c = read(); }
+        do 
+        {
             ret = ret * 10 + c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
-        if (neg)
-            return -ret;
+        } 
+        while ((c = read()) >= '0' && c <= '9');
+        if (neg) { return -ret; }
         return ret;
     }
 
-    public double dbl() throws IOException
+    public double rdouble() throws IOException
     {
         double ret = 0, div = 1;
         byte c = read();
-        boolean eof = false;
-        while (c <= 47 || c >= 58) 
-        {
-            if(c==45) { break; }
-            if(c==-1) { eof = true; break; }
-            c = read();
-        }
-        if(eof) { return Double.NEGATIVE_INFINITY; }
-        
+        while (c <= ' ') { c = read(); }
         boolean neg = (c == '-');
-        if (neg)
-            c = read();
+        if (neg) { c = read(); }
 
-        do {
+        do 
+        {
             ret = ret * 10 + c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
+        } 
+        while ((c = read()) >= '0' && c <= '9');
 
-        if (c == '.') {
-            while ((c = read()) >= '0' && c <= '9') {
+        if (c == '.') 
+        {
+            while ((c = read()) >= '0' && c <= '9') 
+            {
                 ret += (c - '0') / (div *= 10);
             }
         }
-
-        if (neg)
-            return -ret;
+        if (neg) { return -ret; }
         return ret;
     }
 
     private void fillBuffer() throws IOException
     {
-        bytesRead = din.read(buffer, bufferPointer = 0,
-                             BUFFER_SIZE);
-        if (bytesRead == -1)
-            buffer[0] = -1;
+        bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+        if (bytesRead == -1) { buffer[0] = -1; }
     }
 
     private byte read() throws IOException
     {
-        if (bufferPointer == bytesRead)
-            fillBuffer();
+        if (bufferPointer == bytesRead) { fillBuffer(); }
         return buffer[bufferPointer++];
     }
 
     public void close() throws IOException
     {
-        if (din == null)
-            return;
+        if (din == null) { return; }
         din.close();
     }
 }
+
 
